568552291c433f91542f040012b2155a9f45ef69
Kylymbek <kkyly.2000@gmail.com>
2022-04-22 15:42:33 +0600

    finish teacher and learning screen

--- src/pages/EducationPage.js
+++ src/pages/EducationPage.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import {Carousel} from 'react-bootstrap'
+import Contacts from '../containers/Сontacts'
 
 const EducationPage = () => {
     return (
-        <div>
-            education
+      <>
+        <div className={'EducationScreen'}>
+          <Carousel indicators={false} controls={false}>
+              <div className={'Educationimg'}></div>
+              <div className={'EducOverlay'}></div>
+              <div className={'EducationText'}>
+                <div className={'ContainerL'}>
+                  <div className={'EducTitle'}>СИЛЬНОЕ ЕСТЕСТВЕННО-ТЕХНИЧЕСКОЕ ОБРАЗОВАНИЕ</div>
+                  <div className={'EducText'}>Сегодня в мире наблюдается рост спроса на инженеров нового поколения – разработчиков высоких технологий, владеющих математикой, методами моделирования, информатики, управления.</div>
+                </div>
+              </div>
+          </Carousel>
+          <div className={'EducationBread'}>
+            <div className={'EducContainer'}>
+                <span>
+                  <a href={'https://abiturient.kg/'} style={{color:'gray'}}>Главная    > </a>
+                  <span className={'EducBreadCrumbs'} aria-current={'page'}>Сильное естественно-техническое образование</span>
+                </span>
+            </div>
+          </div>
+          <section className={'EducBoxContent'}>
+            <div className={'EducContainer'}>
+              <div className={'EducPageContent'}>
+                <p className={'EducPageText'}>Большинство исследователей, представители общественности и бизнеса считают, что необходимо подготовить массу выпускников с такими умениями и навыками, которые удовлетворят потребность в специализированных трудовых ресурсах XXI века в областях химической технологии и фармакологии, источников энергии и машиностроения, микроэлектроники и средств коммуникации.</p>
+                <p className={'EducPageText'}>Школа «Абитуриент» использует проверенные десятилетиями методики преподавания и программы с высоким теоретическим уровнем. Это позволяет получить такие знания и достичь такого уровня развития мышления детей, которые необходимы для успеха в перечисленных областях.</p>
+                <ul>
+                  <li>Первоочередное внимание уделяется математике – основе для изучения естественно-технических наук и как средству интеллектуального роста.</li>
+                  <li>Насыщенная программа по физике позволяет приобрести достаточно знаний для того, чтобы разобраться в достижениях современной науки и техники.</li>
+                  <li>Обучение химии имеет технологическую направленность и насыщено экспериментом.<a>Ссылка на канал Youtube</a></li>
+                  <li>Курс инженерного конструирования (робототехники) связан с перечисленными науками и с компьютерным программированием. Практика в этом курсе имеет комплексный характер.</li>
+                </ul>
+              </div>
+            </div>
+          </section>
         </div>
+        <Contacts />
+      </>
     );
 };
 
---
+++ src/pages/Example.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+import {Button, Col, Form, Image, Modal, Row} from 'react-bootstrap'
+import logo from '../Image/CaruselImg/malyshok_edu_logo.jpg'
+
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button style={{backgroundColor: '#0466ad'}} onClick={handleShow}>
+        ПОСТУПИТЬ В ШКОЛУ
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Row >
+          <Col md={12} xs={12} style={{padding: 0}}>
+            <div className={'h-100'}
+                 style={{backgroundColor: '#ebebeb', paddingBottom: 40, paddingTop: 40, marginBottom: 10}}>
+              <div className={'mb-5'}>
+                <div style={{display: "flex", justifyContent: "center"}}>
+                  <Image src={logo} style={{width: 100, borderRadius: '100%'}}/>
+                </div>
+                <h4 className="text-center">ОК «МАЛЫШОК»</h4>
+                <p className="text-center">Оставьте заявку и наш менеджер свяжется с Вами, чтобы записать
+                  ребенка на вводное тестирование</p>
+              </div>
+              <Row>
+                <div style={{display: "flex", justifyContent: 'center'}}>
+                  <Col xs={12} md={8}>
+                    <Form className={'mb-2'}>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control type="text" placeholder="Ваше имя *"/>
+                      </Form.Group>
+                    </Form>
+                    <Form className={'mb-2'}>
+                      <Form.Group controlId="formBasicEmail">
+                        <Form.Control type="number" placeholder="Ваш телефон *"/>
+                      </Form.Group>
+                    </Form>
+                    <p>Выберите отделение</p>
+                    <Form.Select aria-label="Default select example" className={'mb-3'}>
+                      <option>Начальная школа</option>
+                      <option value="1">Детский сад</option>
+                      <option value="2">Старшая школа</option>
+                    </Form.Select>
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                      <Button className="btn" style={{
+                        backgroundColor: '#0466ad',
+                        color: '#fff',
+                        fontSize: 16,
+                        fontWeight: "400",
+                        paddingLeft: 40,
+                        paddingRight: 40,
+                        paddingTop: 10,
+                        paddingBottom: 10
+                      }} onClick={handleClose}>ОСТАВИТЬ ЗАЯВКУ
+                      </Button>
+                    </div>
+                  </Col>
+                </div>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Modal>
+    </>
+  );
+}
+
+export default Example
